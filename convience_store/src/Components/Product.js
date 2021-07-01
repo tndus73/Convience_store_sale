848db299c4b64460b1b9bfcb8ce7a1ef41c35527
mingyeongho <1736s@naver.com>
2021-07-02 03:51:45 +0900

    modified css

--- convience_store/src/Components/Product.js
+++ convience_store/src/Components/Product.js
@@ -1,21 +1,27 @@
 import "./Product.scss";
+import { BiWon } from "react-icons/bi";
 
 const Product = ({ product }) => {
   const { idx, name, price, img, etc } = product;
-  if (idx === 1) {
+  if (idx) {
     console.log(idx);
   }
   return (
     <div className="product">
       {img && (
-        <div className="prod_img">
+        <div className="product_img">
           <img src={img} alt="prod_img"></img>
         </div>
       )}
       <div className="content">
-        <p>{name}</p>
-        <p>{price}</p>
-        <p>{etc}</p>
+        <p className="product_name">
+          <strong>{name}</strong>
+        </p>
+        <p className="product_price">
+          <BiWon className="won" />
+          {price}
+        </p>
+        <p className="product_etc">{etc}</p>
       </div>
     </div>
   );
